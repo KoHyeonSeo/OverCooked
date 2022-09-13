98d7a3d5b4af9e4d67b9fba5cbc7f15c6eacdd1a
Kohyeonseo <rhgustj01@naver.com>
2022-09-13 19:53:18 +0900

    Update

---
+++ Assets/Scripts/Moon/Recipe/M_Box.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Box : MonoBehaviour
+{
+    public GameObject getObject; //현재 가지고 있는 오브젝트
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
