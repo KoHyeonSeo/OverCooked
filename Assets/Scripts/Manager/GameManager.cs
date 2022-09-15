94682ff303c279e43ef52cc87ab4eccfab819145
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-09-15 13:46:07 +0900

    Update PlayerAnimation

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -17,12 +17,12 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         //나중에 나의 캐릭터를 찾아야할 방법으로 바꿔야한다.
-        Player = GameObject.Find("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
     private void Update()
     {
         if(!Player)
-            Player = GameObject.Find("Player");
+            Player = GameObject.FindGameObjectWithTag("Player");
     }
     public GameObject Player { get; private set; }
 }
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Chop,
+        Grab,
+        Throw,
+        Die
+    }
+    public State curState = State.Idle;
+    private State saveState = State.Grab;
+    private Animator animator;
+    private void Start()
+    {
+        animator = transform.GetChild(0).GetComponent<Animator>();    
+    }
+    private void Update()
+    {
+        if (curState != saveState)
+        {
+            ChangeState(curState);
+        }
+    }
+
+    private void ChangeState(State curState)
+    {
+        switch (curState)
+        {
+            case State.Idle:
+                animator.SetTrigger("Idle");
+                break;
+            case State.Chop:
+                animator.SetTrigger("Chop");
+                break;
+            case State.Grab:
+                animator.SetTrigger("Grab");
+                break;
+            case State.Throw:
+                animator.SetTrigger("Throw");
+                break;
+            case State.Die:
+                animator.SetTrigger("Die");
+                break;
+        }
+        saveState = curState;
+    }
+
+}
