1091df6335fa52c704731fa2b6da260b2ffc8e00
Kohyeonseo <rhgustj01@naver.com>
2022-09-17 01:27:03 +0900

    Update Deadzone and Add TrashCan

---
+++ Assets/Scripts/Object/TrashCan.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+    //접시나 조리도구 안에 있는 "음식"을 버릴 수 있다.
+    //음식만 들고 있을 때도 음식을 버릴 수 있다.
+}
