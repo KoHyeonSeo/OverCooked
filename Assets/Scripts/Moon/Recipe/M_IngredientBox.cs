124b44c4160c6653d452bf94144f6ce4efa2da64
6108 <73987879+6108@users.noreply.github.com>
2022-09-17 10:02:32 +0900

    test

--- Assets/Scripts/Moon/Recipe/M_IngredientBox.cs
+++ Assets/Scripts/Moon/Recipe/M_IngredientBox.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class M_IngredientBox : MonoBehaviour
 {
     public GameObject ingredientPrefab;
-    //public GameObject ingredient;
+    public GameObject player;
+    bool isPlayerExit;
 
     void Start()
     {
-        
+        if (GameManager.instance.Player)
+        {
+            player = GameManager.instance.Player;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (player && isPlayerExit)
+        {
+            if (Input.GetKeyDown(KeyCode.E))//player.GetComponent<PlayerInput>().LeftClickDown)
+            {
+                print(player.name);
+                GameObject ingredient = Instantiate(ingredientPrefab);
+                ingredient.transform.parent = player.transform;
+                ingredient.transform.localPosition = new Vector3(0, -.5f, .5f);
+                string[] names = ingredient.name.Split('(');
+                ingredient.name = names[0];
+            }
+        }
+        else
+        {
+            player = GameManager.instance.Player;
+        }*/
+    }
+
+    public void CheckClick()
+    {
+        /*if (player.GetComponent<PlayerInput>().LeftClickDown)
+        {
+            GameObject ingredient = Instantiate(ingredientPrefab);
+            ingredient.transform.parent = player.transform;
+            ingredient.transform.localPosition = new Vector3(0, -.5f, .5f);
+            string[] names = ingredient.name.Split('(');
+            ingredient.name = names[0];
+        }*/
     }
 
     //재료 생성
@@ -27,5 +58,27 @@ public class M_IngredientBox : MonoBehaviour
         {
             GameObject ingredient = Instantiate(ingredientPrefab, transform.position, Quaternion.identity);
         }*/
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isPlayerExit = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            isPlayerExit = false;
+        }
     }
 }
