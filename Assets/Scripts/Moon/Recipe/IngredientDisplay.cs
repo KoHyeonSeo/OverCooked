9bd61ad9518561bc1d2abf8b89e9fc938dc1ceeb
Kohyeonseo <rhgustj01@naver.com>
2022-09-22 21:05:36 +0900

    Revert "Revert "Revert "Merge branch 'M'"""
    This reverts commit 6aa425d22f8ee632a52adb7619a729dce4cfef79.

--- Assets/Scripts/Moon/Recipe/IngredientDisplay.cs
+++ Assets/Scripts/Moon/Recipe/IngredientDisplay.cs
@@ -10,67 +10,43 @@ public class IngredientDisplay : MonoBehaviour
     public bool isCut;
     public bool isBake;
     public bool isReady;
-    int modelLevel = 0;
-    int maxModelLevel;
+    int cookLevel = 0;
+    int maxCookLevel;
     GameObject player;
-    Mesh model;
-    Transform modelTransform;
-    Texture modelTexture;
-    BoxCollider modelCollider;
-
     void Start()
     {
         if (GameManager.instance.Player)
         {
             player = GameManager.instance.Player;  
         }
-        //curObject = Instantiate(ingredientObject.cookLevel[cookLevel]);
-        //curObject.transform.position = transform.position;
+        curObject = Instantiate(ingredientObject.cookLevel[cookLevel]);
+        curObject.transform.position = transform.position;
         //curObject.transform.parent = transform;
         //curObject.transform.localPosition = new Vector3(0, -.5f, 0);
         //자르거나 구워야 하는 오브젝트가 아니면 바로 접시에 담을 수 있음
         if (ingredientObject.isPossibleBake)
-            maxModelLevel = 2;
+            maxCookLevel = 2;
         if (ingredientObject.isPossibleCut)
-            maxModelLevel = 1;
-        MeshChange();
-        /*if(!ingredientObject.isPossibleBake && !ingredientObject.isPossibleCut)
+            maxCookLevel = 1;
+        if(!ingredientObject.isPossibleBake && !ingredientObject.isPossibleCut)
         {
             isReady = true;
-        }*/
+        }
     }
-
     public void CookLevelUp()
     {
-        if (modelLevel < maxModelLevel)
-            modelLevel++;
-        MeshChange();
-    }
-
-    void MeshChange()
-    {
-        model = ingredientObject.model[modelLevel].GetComponent<MeshFilter>().sharedMesh;
-        GetComponent<MeshFilter>().sharedMesh = model;
-        modelTransform = ingredientObject.model[modelLevel].GetComponent<Transform>();        
-        GetComponent<Transform>().localScale = modelTransform.localScale;
-        modelTexture = ingredientObject.model[modelLevel].GetComponent<MeshRenderer>().sharedMaterial.mainTexture;
-        GetComponent<MeshRenderer>().material.mainTexture = modelTexture;
-        if (ingredientObject.model[modelLevel].GetComponent<BoxCollider>())
-        {
-            modelCollider = ingredientObject.model[modelLevel].GetComponent<BoxCollider>();
-            GetComponent<BoxCollider>().size = modelCollider.size;
-            GetComponent<BoxCollider>().center = modelCollider.center;
-        }
+        if (cookLevel < maxCookLevel)
+            cookLevel++;
+        Destroy(curObject);
+        //curObject = player.GetComponent<PlayerCreateNew>().CreatesNewObject(ingredientObject.cookLevel[cookLevel], "Default", true, transform, new Vector3(0, 0, 0), true);
+        curObject = Instantiate(ingredientObject.cookLevel[cookLevel]);
+        curObject.transform.position = transform.position;
+        //curObject.transform.parent = transform;
     }
 
     void Update()
     {
         if (!player)
             player = GameManager.instance.Player;
-    }
-
-    private void OnDestroy()
-    {
-        print("1111111111112");
     }
 }
