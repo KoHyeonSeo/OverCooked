8576664e67ea5babe44a468a4ce70a6dd89262e0
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-09-28 16:50:13 +0900

    Update Editor

--- Assets/Editor/BlockEditor.cs
+++ Assets/Editor/BlockEditor.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Codice.Client.BaseCommands;
+using Unity.VisualScripting;
 
 public class BlockEditor : EditorWindow
 {
@@ -13,8 +15,17 @@ public class BlockEditor : EditorWindow
     }
 
     Vector2 scrollPosition;
+
+    //public List<string> objectList = new List<string>();
+
     private void OnGUI()
     {
+        Event e = Event.current;
+        //if (e.type == EventType.MouseDown)
+        //{
+        //    Debug.Log(e.button);
+        //}
+
         GUILayout.Label("Interact Object", EditorStyles.boldLabel);
         Object resource_table = Resources.Load<GameObject>("Editor/Table");
         GUILayout.BeginHorizontal();
@@ -25,6 +36,13 @@ public class BlockEditor : EditorWindow
             GUILayout.MinHeight(220),
             GUILayout.MaxHeight(700));
 
+        GameObject map = GameObject.Find("Object_Parent");
+        if (!map)
+        {
+            map = new GameObject();
+            map.name = "Object_Parent";
+        }
+
         GUILayout.Label("Table");
         if (GUILayout.Button(AssetPreview.GetMiniThumbnail(resource_table)))
         {
@@ -32,16 +50,22 @@ public class BlockEditor : EditorWindow
             GameObject instantiate = Instantiate(resource_table as GameObject);
             instantiate.gameObject.name = instantiate.gameObject.name.Split('(')[0];
             instantiate.transform.position = Vector3.zero;
+            instantiate.transform.parent = map.transform;
         }
+
+
+        GUILayout.Label("Other", EditorStyles.boldLabel); 
 
         GUILayout.Label("Trash Can");
         Object resource_trash = Resources.Load<GameObject>("Editor/Trash Can");
+
         if (GUILayout.Button(AssetPreview.GetMiniThumbnail(resource_trash)))
         {
             EditorGUIUtility.PingObject(resource_trash);
             GameObject instantiate = Instantiate(resource_trash as GameObject);
             instantiate.gameObject.name = instantiate.gameObject.name.Split('(')[0];
             instantiate.transform.position = Vector3.zero;
+            instantiate.transform.parent = map.transform;
         }
 
         GUILayout.Label("Fire Extinguisher");
@@ -52,21 +76,77 @@ public class BlockEditor : EditorWindow
             GameObject instantiate = Instantiate(resource_FireExtinguisher as GameObject);
             instantiate.gameObject.name = instantiate.gameObject.name.Split('(')[0];
             instantiate.transform.position = Vector3.zero;
+            instantiate.transform.parent = map.transform;
         }
 
-        GUILayout.Label("Other", EditorStyles.boldLabel); 
-
-        GUILayout.Label("Deadzone");
-        Object resource_DeadZone = Resources.Load<GameObject>("Editor/Deadzone");
-        if (GUILayout.Button(AssetPreview.GetMiniThumbnail(resource_DeadZone)))
-        {
-            EditorGUIUtility.PingObject(resource_DeadZone);
-            GameObject instantiate = Instantiate(resource_DeadZone as GameObject);
-            instantiate.gameObject.name = instantiate.gameObject.name.Split('(')[0];
-            instantiate.transform.position = Vector3.zero;
-        }
         GUILayout.FlexibleSpace();
         GUILayout.EndScrollView();
         GUILayout.EndHorizontal();
+    }
+}
+
+
+[CustomEditor(typeof(Map))]
+public class BlockEditWithEditor : Editor
+{
+    Map map;
+    private void OnEnable()
+    {
+        map = (Map)target;
+    }
+    //우클릭하면 선택 & 위치 변경 가능
+    //좌클릭하고 드래그 -> 여러개 생성
+    //우클릭해서 선택 상태일때, 마우스 휠 -> 물체 변경
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        map.tileX = EditorGUILayout.IntField("타일 가로", map.tileX);
+        map.tileZ = EditorGUILayout.IntField("타일 세로", map.tileZ);
+
+        map.tileX = Mathf.Clamp(map.tileX, 1, 500);
+        map.tileZ = Mathf.Clamp(map.tileZ, 1, 500);
+
+        map.floorTile = (GameObject)EditorGUILayout.ObjectField("타일", map.floorTile, typeof(GameObject), false);
+
+        if(GUILayout.Button("바닥 생성"))
+        {
+            CreateFloor();
+        }
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("블록 모두 삭제"))
+        {
+            bool isCancel = EditorUtility.DisplayDialog(" 주의 ", "\n정말로 모두 삭제하시겠습니까?", "OK", "CANCEL");
+            if(isCancel)
+                ClearMapObjects();
+        }
+
+    }
+    private void OnSceneGUI()
+    {
+        int id = GUIUtility.GetControlID(FocusType.Passive);
+        HandleUtility.AddDefaultControl(id);
+    }
+    /// <summary>
+    /// 바닥 생성 함수
+    /// </summary>
+    void CreateFloor()
+    {
+        GameObject floor = GameObject.Find("Tile");
+        if (floor)
+        {
+            DestroyImmediate(floor);
+        }
+        floor = (GameObject)PrefabUtility.InstantiatePrefab(map.floorTile);
+        floor.transform.localScale = new Vector3(map.tileX, 1, map.tileZ);
+    }
+    /// <summary>
+    /// 맵에 배치한 오브젝트 모두 삭제하는 함수
+    /// </summary>
+    void ClearMapObjects()
+    {
+        GameObject obj = GameObject.Find("Object_Parent");
+        DestroyImmediate(obj);
     }
 }
---
+++ Assets/Scripts/Other/Map.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public int tileX;
+    public int tileZ;
+
+    public GameObject floorTile;
+}
