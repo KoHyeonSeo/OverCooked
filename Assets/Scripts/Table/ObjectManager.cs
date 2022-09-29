c6ad192bc2affe39d5790cedb73cf04327235fde
6108 <73987879+6108@users.noreply.github.com>
2022-09-29 13:14:48 +0900

    FryingPanPhotonError

--- Assets/Scripts/Table/ObjectManager.cs
+++ Assets/Scripts/Table/ObjectManager.cs
@@ -16,7 +16,6 @@ public class ObjectManager : MonoBehaviourPun
 
     public void SetPhotonObject(GameObject obj)
     {
-        print(obj.GetComponent<PhotonView>().ViewID);
         photonObjectIdList.Add(obj);
     }
 }
