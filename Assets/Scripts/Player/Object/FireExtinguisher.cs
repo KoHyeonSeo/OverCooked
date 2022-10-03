f4af0c1ef267f13e67e22f059ceb1aa88fcc3908
Kohyeonseo <rhgustj01@naver.com>
2022-10-03 13:18:30 +0900

    Update Fire Extinguisher

--- Assets/Scripts/Player/Object/FireExtinguisher.cs
+++ Assets/Scripts/Player/Object/FireExtinguisher.cs
@@ -11,6 +11,7 @@ public class FireExtinguisher : MonoBehaviourPun, IPunObservable
     private Vector3 recievePos;
     private Quaternion recieveRot;
 
+    private bool isFireParticle = false;
 
     private void Start()
     {
@@ -24,23 +25,33 @@ public class FireExtinguisher : MonoBehaviourPun, IPunObservable
             player = GameManager.instance.Player;
         if (photonView.IsMine)
         {
-            if (player.GetComponent<PlayerInteract>().curInteractState == PlayerInteract.InteractState.FireDistinguish
-                && player.GetComponent<PlayerInput>().FireExtinguisher)
+            if (player.transform.childCount> 1
+                && player.transform.GetChild(1).name==transform.name
+                )
             {
-                Ray ray = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.forward);
-
-                //소화기 쏘는 부분이 닿았니
-                Debug.DrawRay(ray.origin, ray.direction * 5, Color.magenta);
-                if (Physics.Raycast(ray, out hit, 5))
+                if (player.GetComponent<PlayerInput>().FireExtinguisher)
                 {
-                    if (hit.transform.name.Contains("FireTable"))
+                    Ray ray = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.forward);
+
+                    photonView.RPC("FireParticle", RpcTarget.All, true);
+
+                    //소화기 쏘는 부분이 닿았니
+                    Debug.DrawRay(ray.origin, ray.direction * 5, Color.magenta);
+                    if (Physics.Raycast(ray, out hit, 5))
                     {
-                        if (hit.transform.GetComponent<FireBox>().isFire)
+                        if (hit.transform.name.Contains("FireTable"))
                         {
-                            //hit.transform.GetComponent<FireBox>().FireSuppression(extinguisher * Time.deltaTime);
-                            photonView.RPC("FireSuppression", RpcTarget.All, (extinguisher * Time.deltaTime));
+                            if (hit.transform.GetComponent<FireBox>().isFire)
+                            {
+                                //hit.transform.GetComponent<FireBox>().FireSuppression(extinguisher * Time.deltaTime);
+                                photonView.RPC("FireSuppression", RpcTarget.All, (extinguisher * Time.deltaTime));
+                            }
                         }
                     }
+                }
+                else
+                {
+                    photonView.RPC("FireParticle", RpcTarget.All, false);
                 }
             }
         }
@@ -49,13 +60,22 @@ public class FireExtinguisher : MonoBehaviourPun, IPunObservable
             transform.position = Vector3.Lerp(transform.position, recievePos, Time.deltaTime * 7);
             transform.rotation = Quaternion.Lerp(transform.rotation, recieveRot, Time.deltaTime * 7);
         }
+
     }
     [PunRPC]
     public void FireSuppression(float ex)
     {
         hit.transform.GetComponent<FireBox>().FireSuppression(ex);
     }
-
+    [PunRPC]
+    public void FireParticle(bool isFire)
+    {
+        if (transform.childCount == 1)
+        {
+            if (transform.GetChild(0).childCount == 1)
+                transform.GetChild(0).GetChild(0).gameObject.SetActive(isFire);
+        }
+    }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (transform.parent != null) return;
